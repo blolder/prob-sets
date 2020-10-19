d2c8f879129aa3a0204995d45133e26b58e48aa0
HiMolde logistikk course, first semester <54948393+HimoldeSCM1@users.noreply.github.com>
2020-10-19 14:16:18 +0200

    Fix

--- plot.py
+++ plot.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 plt.autoscale(True)
 plt.plot([100, 300, 500, 700, 900, 1100, 1300, 1500, 1700, 1900, 2100, 2200, 2300], [100, 300, 500, 700, 900, 1100, 1300, 1500, 1700, 1900, 2100, 2200, 2300])
 plt.plot([100, 300, 500, 700, 900, 1100, 1300, 1500, 1700, 1900, 2100, 2200, 2300], [10, 30, 50, 70, 90, 110, 130, 150, 170, 190, 210, 220, 230])
-plt.xlabel('Number of files')
+plt.xlabel('N')
 plt.ylabel('Time in seconds')
 plt.show()
 
@@ -56,5 +56,5 @@ def run_algorithm(algorithm, n):
 
 for n in range(10, 1000, 100):
     times = min(run_algorithm("factorial", n))
-    print (times) #seconds
+    print (times) #ms
 
